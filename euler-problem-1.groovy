d99435895c3658ed1964299c261b7e01ca687506
Masato Nagai <me@masatonagai.com>
2013-05-18 17:33:01 +0900

    update

--- euler-problem-1.groovy
+++ euler-problem-1.groovy
@@ -1,1 +1,4 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
 println((3..<1000).inject(0) { sum, n -> if (n % 3 == 0 || n % 5 == 0) { sum + n } else { sum } })
