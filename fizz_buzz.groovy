bd1974109479c4b5b365a0880d68b0d6adbd14a2
Masato Nagai <me@masatonagai.com>
2013-05-19 00:43:37 +0900

    update

--- fizz_buzz.groovy
+++ fizz_buzz.groovy
@@ -1,7 +1,8 @@
-1.upto(100) { n ->
-  println(
-    (n % 15 == 0) ? "fizzbuzz"
-      : (n % 3 == 0) ? "fizz"
-        : (n % 5 == 0) ? "buzz"
-          : n)
+def r = (1..100).inject([]) { r, n ->
+  r << (
+    (n % 15 == 0) ? "fizzbuzz" :
+    (n % 3 == 0) ? "fizz" :
+    (n % 5 == 0) ? "buzz" :
+    n)
 }
+println(r.join('\n'))
